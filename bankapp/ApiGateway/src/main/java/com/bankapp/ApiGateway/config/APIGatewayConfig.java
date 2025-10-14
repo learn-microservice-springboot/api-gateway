3fd3316e9c8488e7b4311b67a678ab925d440fc2
Sayan Mallick <sayan4326@gmail.com>
2025-10-14 21:43:50 +0530

    Add existing filter of adding response header from api gateway

--- bankapp/ApiGateway/src/main/java/com/bankapp/ApiGateway/config/APIGatewayConfig.java
+++ bankapp/ApiGateway/src/main/java/com/bankapp/ApiGateway/config/APIGatewayConfig.java
@@ -1,5 +1,7 @@
 package com.bankapp.ApiGateway.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,14 +14,16 @@ public class APIGatewayConfig {
 	RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
 		return routeLocatorBuilder.routes()
 				.route("customer-service-route", r -> r.path("/api/bankapp/customer/**")
-						.filters(f -> f.rewritePath("/api/bankapp/customer(?<segment>.*)", "/api/customer${segment}"))
+						.filters(f -> f.rewritePath("/api/bankapp/customer(?<segment>.*)", "/api/customer${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CUSTOMERSERVICE"))
 				.route("loan-service-route",
 						r -> r.path("/api/bankapp/loans/**")
-								.filters(f -> f.rewritePath("/api/bankapp/loans(?<segment>.*)", "/api/loans${segment}"))
+								.filters(f -> f.rewritePath("/api/bankapp/loans(?<segment>.*)", "/api/loans${segment}")
+										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 								.uri("lb://LOANSERVICE"))
 				.build();
-	
+
 	}
 
 }
